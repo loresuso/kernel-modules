a879c921d1299214ef987a3ab4cddb05d345f573
loresuso <susinilorenzo1@gmail.com>
2021-03-10 09:05:21 -0800

    qemu

--- qemu-old/bin/x86_64/pc-bios/optionrom/linuxboot.d
+++
@@ -1,4 +0,0 @@
-linuxboot.o: /home/lore/MasterThesis/qemu/pc-bios/optionrom/linuxboot.S \
- /home/lore/MasterThesis/qemu/pc-bios/optionrom/optionrom.h
-
-/home/lore/MasterThesis/qemu/pc-bios/optionrom/optionrom.h:
--- qemu-old/bin/x86_64/pc-bios/optionrom/pvh.d
+++
@@ -1,4 +0,0 @@
-pvh.o: /home/lore/MasterThesis/qemu/pc-bios/optionrom/pvh.S \
- /home/lore/MasterThesis/qemu/pc-bios/optionrom/optionrom.h
-
-/home/lore/MasterThesis/qemu/pc-bios/optionrom/optionrom.h:
