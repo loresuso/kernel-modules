43512250ba359221ee9d877ef34dd963a9f59aab
loresuso <susinilorenzo1@gmail.com>
2021-03-31 02:34:01 -0700

    ok

--- qemu/bin/x86_64/pc-bios/optionrom/kvmvapic.d
+++
@@ -1,4 +0,0 @@
-kvmvapic.o: /home/lore/MasterThesis/qemu/pc-bios/optionrom/kvmvapic.S \
- /home/lore/MasterThesis/qemu/pc-bios/optionrom/optionrom.h
-
-/home/lore/MasterThesis/qemu/pc-bios/optionrom/optionrom.h:
--- qemu/bin/x86_64/pc-bios/optionrom/multiboot.d
+++
@@ -1,4 +0,0 @@
-multiboot.o: /home/lore/MasterThesis/qemu/pc-bios/optionrom/multiboot.S \
- /home/lore/MasterThesis/qemu/pc-bios/optionrom/optionrom.h
-
-/home/lore/MasterThesis/qemu/pc-bios/optionrom/optionrom.h:
